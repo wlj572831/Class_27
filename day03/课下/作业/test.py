f38bb3106d0a4b1257e6230c05a0fb316e6c629f
home <wlj572831@163.com>
2019-07-27 07:06:07 +0800

    7.27凌晨2

--- day03/课下/作业/test.py
+++ day03/课下/作业/test.py
@@ -4,12 +4,12 @@
 
 
 
-title_list = ['序号', '代码', '名称', '最新价', '涨跌幅', '涨跌额', '成交量', '成交额', '振幅', '最高', '最低', '今开', '昨收', '量比', '换手率', '市盈率',
-              '市净率']
-obj = input('')
-tu = obj.split('>')
-print(tu)
-find_index = title_list.index('振幅')
-if ('亿' in tu[1]) or ('万' in tu[1]) or ('%' in tu[1]):  # 1
-    print('1')
-print(find_index)
+a = [1.22, 1.33]
+
+def abc(a):
+    if '万' in a:
+        return
+    return a
+
+
+abc(a)
--- day03/课下/作业/股票查询程序开发.py
+++ day03/课下/作业/股票查询程序开发.py
@@ -44,22 +44,24 @@ def to_num(obj):
     if '%' in obj:
         obj = obj.replace('%', '')
         obj = float(obj)
-        obj /= 100
-
+        return obj
+    result = obj
+    return result
 # 6.列表内容格式化
 '''
-1. 把万、亿'%'统一转换成数字或者浮点型类型
+1. 把万、亿统一转换成数字或者浮点型类型
 2. 把数字、float类型的进行转换,第一列序号和第二列编号不转换
 3. 换手率中有 '-'标识符，方便比较，统一转换成0
 '''
 for i in range(len(cont_list)):
     for j in range(3, len(cont_list[i])):
-        if ('亿' in cont_list[i][j]) or ('万' in cont_list[i][j]) or('%' in cont_list[i][j]) : #1
-            cont_list[i][j] = to_num(cont_list[i][j])
+        # if ('亿' in cont_list[i][j]) or ('万' in cont_list[i][j]): #1
+        #     cont_list[i][j] = to_num(cont_list[i][j])
         if is_float(cont_list[i][j]):   #2
             cont_list[i][j] = float(cont_list[i][j])
         if cont_list[i][j] == '-':  #3
             cont_list[i][j] = 0
+
 # 7.模糊查询
 def fuzzy_(name):
     find_result = []
@@ -69,19 +71,25 @@ def fuzzy_(name):
     return find_result, len(find_result)
 
 # 8.比较查询
+'''
+1.将输入的结果根据<>分割成“查询项”和数量对比，根据查询项在表头内搜索，获取到下标，根据下标去列表中取相应的数值
+2.两者进行比较，将符合条件的加入新列表返回
+'''
 def compare(obj):
     if obj.count('<') == 1:
         tu = obj.split('<')
         for i in tu:i=i.strip()
         if tu[0] in title_list:
             find_index = title_list.index(tu[0])
+            if '%' in tu[1]:
+                tu[1] = tu[1].strip('%')
             if ('亿' in tu[1]) or ('万' in tu[1]) or ('%' in tu[1]):  # 1
                 tu[1] = to_num(tu[1])
             if is_float(tu[1]):
                 tu[1] = float(tu[1])
             find_result = []
             for i in cont_list:
-                if i[find_index] < tu[1]:
+                if to_num(i[find_index]) < tu[1]:
                     find_result.append(i)
             return find_result, len(find_result)
         else:
@@ -90,15 +98,14 @@ def compare(obj):
         tu = obj.split('>')
         if tu[0] in title_list:
             find_index = title_list.index(tu[0])
-            if ('亿' in tu[1]) or ('万' in tu[1]) or ('%' in tu[1]):  # 1
-                tu[1] = to_num(tu[1])
-            print(tu[1])
+            # if ('亿' in tu[1]) or ('万' in tu[1])or ('%' in tu[1]):  # 1
+            #     tu[1] = to_num(tu[1])
             if is_float(tu[1]):
                 tu[1] = float(tu[1])
-            print(tu[1])
             find_result = []
             for i in cont_list:
-                if i[find_index] > tu[1]:
+                print(type(i[find_index]), type(tu[1]))
+                if to_num(i[find_index]) > to_num(tu[1]):
                     find_result.append(i)
             return find_result, len(find_result)
         else:
