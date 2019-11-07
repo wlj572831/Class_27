323813d9e6210733efa1bfe9b8ddaa88d7675367
wlj <wlj572831@163.com>
2019-11-07 17:34:01 +0800

    delete完成

--- homework/bookmanager/app01/views.py
+++ homework/bookmanager/app01/views.py
@@ -20,13 +20,15 @@ def add_pub(request):
         if not pub_name:
             return render(request, 'add_pub.html', {'error': '输入的名称不能为空'})
         ret = models.Publisher.objects.create(name=pub_name)
-        print(ret, type(ret))
         return redirect('/show_publist')  # 重定向urls中的第一个元素
     return render(request, 'add_pub.html')
 
 
 def del_pub(request):
-    return render(request, 'publist.html')
+    pk = request.GET.get('pk')
+    obj = models.Publisher.objects.filter(pk=pk).delete()  # 删除查询到的数据
+    print(obj)
+    return redirect('/show_publist/')
 
 
 def index(request):
